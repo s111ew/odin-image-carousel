0a22a02608155a2db1f6899e6cc7dde8e57fb77e
s111ew <167631312+s111ew@users.noreply.github.com>
2024-09-18 15:25:42 +0100

    Initialise webpack in the directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,38 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-mode: "development",
-entry: "./src/index.js",
-output: {
-filename: "main.js",
-path: path.resolve(\_\_dirname, "dist"),
-clean: true,
-},
-devtool: "eval-source-map",
-devServer: {
-watchFiles: ["./src/template.html"],
-},
-plugins: [
-new HtmlWebpackPlugin({
-template: "./src/template.html",
-}),
-],
-module: {
-rules: [
-{
-test: /\.css$/i,
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"), // Corrected __dirname
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: {
+      paths: ["./src/template.html"], // Specify paths for watchFiles
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/i,
-loader: "html-loader",
-},
-{
-test: /\.(png|svg|jpg|jpeg|gif)$/i,
-type: "asset/resource",
-},
-],
-},
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
